08e2fddec546859b89eda96a735f07ea7111461d
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2023-05-12 23:55:32 -0400

    refactored code from main into seperate classes

---
+++ src/main/kotlin/AudioLine/audioLine.kt
@@ -0,0 +1,18 @@
+package audio
+
+import audioFormat
+
+import javax.sound.sampled.AudioFormat
+import javax.sound.sampled.AudioSystem
+import javax.sound.sampled.DataLine
+import javax.sound.sampled.SourceDataLine
+
+
+class AudioLine () {
+    private val dataLineInfo = DataLine.Info(SourceDataLine::class.java, audioFormat)
+    private val line = AudioSystem.getLine(dataLineInfo) as SourceDataLine
+
+    public fun getLine(): SourceDataLine {
+        return line
+    }
+}
